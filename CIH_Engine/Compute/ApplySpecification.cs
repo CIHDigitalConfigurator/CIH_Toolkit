6af1bf6589ef1009b2f0a513972a70c2ee0ebd65
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-01-14 16:43:10 +0000

    Creation of initial Specification and Checks prototype

---
+++ CIH_Engine/Compute/ApplySpecification.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Data.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.CIH
+{
+    public static partial class Compute
+    {
+        public static List<object> ApplySpecification(List<object> objects, ISpecification specification)
+        {
+            List<object> filteredObjects = ApplyFilter(objects, specification.Filter);
+            List<>
+        }
+    }
+}
