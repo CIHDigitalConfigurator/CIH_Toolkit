a1bb44a8e75866d60bd71c050cfcfac06fcee683
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-01-14 16:43:10 +0000

    Creation of initial Specification and Checks prototype

---
+++ CIH_Engine/Compute/ApplyCheck.cs
@@ -0,0 +1,17 @@
+﻿using BH.oM.Data.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.CIH
+{
+    public static partial class Compute
+    {
+        public static List<object> ApplyCheck(List<object> objects, ISpecification specification)
+        {
+            return null;
+        }
+    }
+}
