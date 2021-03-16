03005c686f9b5bd21bbd64866bd8560695c72ac6
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-03-16 15:41:06 +0000

    Added Create method for DomainCondition

---
+++ CIH_Engine/Create/DomainCondition.cs
@@ -0,0 +1,17 @@
+﻿using BH.oM.Data.Conditions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIH_Engine
+{
+    public partial class Create
+    {
+        public DomainCondition DomainCondition(double min, double max)
+        {
+            return new DomainCondition() { Domain = BH.Engine.Data.Create.Domain(min, max) };
+        }
+    }
+}
